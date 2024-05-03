993ac5cc39c83fcdcbcbcd8a573af78a8cb211b5
Redpro <nesho1616@gmail.com>
2024-05-03 17:48:43 +0100

    day4 evening done

--- src/test/java/com/cydeo/utils/BrowserUtils.java
+++ src/test/java/com/cydeo/utils/BrowserUtils.java
@@ -1,26 +1,31 @@
 package com.cydeo.utils;
 
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.ElementState;
 
 public class BrowserUtils {
 
-    public  static  void sleepWithThread(int seconds){
+    public static void sleepWithThread(int seconds) {
 
 
         try {
-            Thread.sleep(seconds*1000);
+            Thread.sleep(seconds * 1000);
 
-        }catch (Exception e){
+        } catch (Exception e) {
 
         }
 
     }
 
-    public static void sleepWithPage(Page page,int seconds){
-        page.waitForTimeout(seconds*1000);
+    public static void sleepWithPage(Page page, int seconds) {
+        page.waitForTimeout(seconds * 1000);
 
     }
 
+    public static void waitForInvisibilityOfElement(Locator locator) {
+        locator.elementHandle().waitForElementState(ElementState.HIDDEN);
+    }
 
 
 }
